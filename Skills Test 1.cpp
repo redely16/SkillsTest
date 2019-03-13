80c163554581e542589d28a1c9fa2ae876e5fc30
redely16 <47804382+redely16@users.noreply.github.com>
2019-03-13 13:06:59 +0800

    Add files via upload

---
+++ Skills Test 1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main ()
+
+{
+	double wageperhour;
+	cout << " Pay Rate per hor:  " ;
+	cout << wageperhour;
+	cout << wageperhour << " $" << " per Hour" << endl;
+
+	int hours;
+	cout << "Hours of Work per week: ";
+	cin >> hours;
+	cout << hours << "  " << "hours a week" << endl;
+	cout << "           " << endl;
+
+
+	int beforeincome;
+	cout << beforeincome;
+	cout << " A Total Income: $" << wageperHour * hours << "   " << endl;
+	
+	int taxincome;
+	cout << taxincome;
+	cout << " Net Income: $" << (beforeincome) - (beforeincome * 0.14) << " " << endl;
+	cout << "       " << endl;
+
+	int clothesandaccessories;
+	cout << "Clothes and Accessories" << (taxincome) -(taxincome * 0.10) << "  " << endl;
+	cout << "      " << endl; 
+
+	int schoolsuppliesl;
+	cout << "School Supplies" << (taxincome) -(taxincome * 0.01) << "  " << endl;
+	cout << "      " << endl; 
+
+	int Moneyspend;
+	cout << "Money Spend" << (clothesandaccessories + schoolsupplies) << "  " endl;
+	cout << "      " << endl; 
+
+
+	int Remainingmoney
+	cout << "Remaining Money" << (Netincome
+
+
+
+
+
+
+_getch ();
+return 0;
+}
+
